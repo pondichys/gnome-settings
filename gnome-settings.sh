4f1e0f5440afa085bfed3bf945c8da880cb2d114
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-05-15 08:18:10 +0200

    feat: add basic gnome configs

---
+++ gnome-settings.sh
@@ -0,0 +1,7 @@
+gsettings set org.gnome.desktop.interface clock-format '24h'
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
